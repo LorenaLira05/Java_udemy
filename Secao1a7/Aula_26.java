ad18abb4219c0600c36c646dc47c9573a3cd961a
Lorena Lira <naheelee2005@gmail.com>
2025-10-16 14:12:31 -0300

    Organizando as pastas

---
+++ Secao1a7/Aula_26.java
@@ -0,0 +1,17 @@
+package cursoJava4;
+
+import java.util.Scanner;
+
+public class Aula_26 {
+	public static void main(String[] args ) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		x = sc.nextInt();	
+		System.out.println("Você digitou: " + x);
+		
+		sc.close();
+		
+	}
+}
